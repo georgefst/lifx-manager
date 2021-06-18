795380290177877081df1d37bcd9c242700625f7
George Thomas <georgefsthomas@gmail.com>
2021-06-18 17:32:11 +0100

    Scan for devices at start, and allow switching between them

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,14 @@ module Main (main) where
 import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Trans.Maybe
 import Data.Colour.RGBSpace
 import Data.Colour.RGBSpace.HSV (hsv)
 import Data.List.Extra
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.Stream.Infinite (Stream)
+import Data.Stream.Infinite qualified as Stream
+import Data.Text.Encoding (decodeUtf8)
 import Data.Tuple.Extra
 import Data.Word
 import Graphics.Gloss
@@ -16,12 +21,11 @@ import Network.Socket
 import Optics hiding (both)
 import Optics.State.Operators
 import Options.Generic
+import System.Exit
 import Text.Pretty.Simple (pPrint)
 
 data Opts = Opts
-    { -- | local address of target light
-      ip :: Ip
-    , -- | 0 to 1
+    { -- | 0 to 1
       width :: Float
     , -- | 0 to 1
       height :: Float
@@ -58,6 +62,8 @@ data AppState = AppState
     { hsbk :: HSBK
     , -- | Which axis, if any, we are currently moving.
       dimension :: Maybe ColourDimension
+    , -- | All devices. Head is the active device.
+      devices :: Stream (Text, HostAddress)
     }
     deriving (Show, Generic)
 
@@ -67,11 +73,19 @@ main = do
     (screenWidth, screenHeight) <- both fromIntegral <$> getScreenSize
     let windowWidth = screenWidth * width
         windowHeight = screenHeight * height
-    (e, s0, LightState{hsbk = colour0}) <-
-        runLifx $
+    initData <-
+        runLifx . runExceptT $
             (,,) <$> ((,,) <$> getSocket <*> getSource <*> getTimeout)
                 <*> getCounter
-                <*> sendMessage (unIp ip) GetColor
+                <*> do
+                    discoverDevices' >>= \case
+                        dev0 : devs' -> do
+                            LightState{hsbk = colour0} <- sendMessage (snd dev0) GetColor
+                            pure (dev0 :| devs', colour0)
+                        _ -> ExceptT $ pure $ Left "timed out without finding any devices!"
+    (e, s0, (devs, colour0)) <- case initData of
+        Right r -> pure r
+        Left e -> putStrLn e >> exitFailure
     interactM
         ( \(a, (_e, s)) x ->
             runExceptT (runReaderT (runStateT (unLifxT (runStateT x a)) s) e) >>= \case
@@ -88,9 +102,9 @@ main = do
             )
         )
         white
-        (AppState colour0 Nothing, (e, s0))
+        (AppState colour0 Nothing (Stream.cycle devs), (e, s0))
         (pure . render lineWidthProportion columns (windowWidth, windowHeight) . fst)
-        (update windowWidth $ unIp ip)
+        (update windowWidth)
         mempty
   where
     f a ((b, c), d) = (b, (c, (a, d)))
@@ -129,24 +143,31 @@ render lineWidthProportion (fromIntegral -> columns) (w, h) AppState{..} =
     rectHeight = h / 4
     columnWidth = w / columns
 
-update :: Float -> HostAddress -> Event -> StateT AppState Lifx ()
-update w addr = \case
-    EventKey (MouseButton LeftButton) Up _ _ -> sendMessage addr $ SetPower True
-    EventKey (MouseButton RightButton) Up _ _ -> sendMessage addr $ SetPower False
-    EventKey (SpecialKey KeySpace) Down _ _ ->
-        sendMessage addr . SetPower . (== 0) . view #power =<< sendMessage addr GetPower
-    EventKey (Char 'h') Down _ _ -> #dimension .= Just H
-    EventKey (Char 's') Down _ _ -> #dimension .= Just S
-    EventKey (Char 'b') Down _ _ -> #dimension .= Just B
-    EventKey (Char 'k') Down _ _ -> #dimension .= Just K
-    EventKey (SpecialKey KeyEsc) Down _ _ -> #dimension .= Nothing
-    EventMotion (clamp (0, 1) . (+ 0.5) . (/ w) -> x, _y) ->
-        gets (view #dimension) >>= maybe (pure ()) \d -> do
-            let l = fromIntegral $ cdLower d
-                u = fromIntegral $ cdUpper d
-            #hsbk % cdLens d .= round (u * x - l * (x - 1))
-            sendMessage addr . flip SetColor (Duration 0) =<< gets (view #hsbk)
-    _ -> pure ()
+update :: Float -> Event -> StateT AppState Lifx ()
+update w event = do
+    addr <- gets $ snd . streamHead . view #devices
+    case event of
+        EventKey (MouseButton LeftButton) Up _ _ -> sendMessage addr $ SetPower True
+        EventKey (MouseButton RightButton) Up _ _ -> sendMessage addr $ SetPower False
+        EventKey (SpecialKey KeySpace) Down _ _ ->
+            sendMessage addr . SetPower . (== 0) . view #power =<< sendMessage addr GetPower
+        EventKey (Char 'h') Down _ _ -> #dimension .= Just H
+        EventKey (Char 's') Down _ _ -> #dimension .= Just S
+        EventKey (Char 'b') Down _ _ -> #dimension .= Just B
+        EventKey (Char 'k') Down _ _ -> #dimension .= Just K
+        EventKey (SpecialKey KeyEsc) Down _ _ -> #dimension .= Nothing
+        EventMotion (clamp (0, 1) . (+ 0.5) . (/ w) -> x, _y) ->
+            gets (view #dimension) >>= maybe (pure ()) \d -> do
+                let l = fromIntegral $ cdLower d
+                    u = fromIntegral $ cdUpper d
+                #hsbk % cdLens d .= round (u * x - l * (x - 1))
+                sendMessage addr . flip SetColor (Duration 0) =<< gets (view #hsbk)
+        EventKey (Char 'l') Down _ _ -> do
+            #devices %= Stream.tail
+            (_name, addr') <- streamHead <$> use #devices
+            LightState{hsbk} <- sendMessage addr' GetColor
+            #hsbk .= hsbk
+        _ -> pure ()
 
 {- Util -}
 
@@ -164,18 +185,6 @@ interactM ::
     (Controller -> IO ()) ->
     IO ()
 interactM trans d c s0 v u = interactIO d c s0 v (flip \s -> fmap (maybe s snd) . trans s . u)
-
-newtype Ip = Ip {unIp :: HostAddress}
-    deriving stock (Generic)
-    deriving anyclass (ParseRecord, ParseField, ParseFields)
-instance Show Ip where
-    show (Ip x) = intercalate "." $ map show [a, b, c, d]
-      where
-        (a, b, c, d) = hostAddressToTuple x
-instance Read Ip where
-    readsPrec _ s = case map read $ splitOn "." s of
-        [a, b, c, d] -> pure . (,"") . Ip $ tupleToHostAddress (a, b, c, d)
-        _ -> []
 
 rgbToGloss :: RGB Float -> Color
 rgbToGloss RGB{..} =
@@ -195,3 +204,13 @@ hsbkToRgb HSBK{..} =
 
 maxWord16 :: Float
 maxWord16 = fromIntegral $ maxBound @Word16
+
+--TODO for some reason, there is no Stream.head: https://github.com/ekmett/streams/pull/19
+streamHead :: Stream a -> a
+streamHead = (Stream.!! 0)
+
+discoverDevices' :: MonadLifx m => m [(Text, Word32)]
+discoverDevices' =
+    discoverDevices
+        >>= traverse
+            (\addr -> (,addr) . decodeUtf8 . view #label <$> sendMessage addr GetColor)
