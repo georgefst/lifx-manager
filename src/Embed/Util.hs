22df9dfbdf219a8bf591dad572a63c8fa1018192
George Thomas <georgefsthomas@gmail.com>
2021-12-26 18:11:00 +0000

    Move embedded images to subfolder

--- src/Embed/Util.hs
+++ src/Embed/Util.hs
@@ -2,6 +2,7 @@ module Embed.Util where
 
 import Data.FileEmbed (embedFile)
 import Language.Haskell.TH (Exp, Q)
+import System.FilePath ((</>))
 
 embed :: FilePath -> Q Exp
-embed = embedFile
+embed = embedFile . ("embed" </>)
