5bdbca2da774ccba6fbb3b0f864ea860a7e218c6
George Thomas <georgefsthomas@gmail.com>
2023-11-04 22:41:42 +0000

    Add clarifying comment to `embed`

--- src/Embed/Util.hs
+++ src/Embed/Util.hs
@@ -4,5 +4,6 @@ import Data.FileEmbed (embedFile)
 import Language.Haskell.TH (Exp, Q)
 import System.FilePath ((</>))
 
+-- | Needs to be in its own module due to the phase restriction.
 embed :: FilePath -> Q Exp
 embed = embedFile . ("embed" </>)
