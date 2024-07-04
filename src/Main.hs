georgefst/lifx-manager

commit e957c05d650d49cf91c161cb22fab32d8852d781

Fix click behaviour on brightness-only devices
Previously we were implicitly assuming that `dims` was a prefix of `enumerate`.

As a bonus, this new code is less repetitive anyway.

src/Main.hs
@@ -406,13 +406,9 @@ update inc event = do
             -- TODO synchronise this with rendering
             let bottomRowCols = 3
                 bottomRowHeight = 1 / 4
+                row = rows - ceiling (fromIntegral rows * (y - bottomRowHeight) / (1 - bottomRowHeight))
              in if
-                    | y > bottomRowHeight + (4 / rows * (1 - bottomRowHeight)) -> #lastError ?= OutOfRangeY y
-                    | y > bottomRowHeight + (3 / rows * (1 - bottomRowHeight)) -> setColour H
-                    | y > bottomRowHeight + (2 / rows * (1 - bottomRowHeight)) -> setColour S
-                    | y > bottomRowHeight + (1 / rows * (1 - bottomRowHeight)) -> setColour B
-                    | y > bottomRowHeight + (0 / rows * (1 - bottomRowHeight)) -> setColour K
-                    | y >= 0 ->
+                    | row == rows ->
                         if
                             | x > 3 / bottomRowCols -> #lastError ?= OutOfRangeX x
                             | x > 2 / bottomRowCols ->
@@ -424,9 +420,10 @@ update inc event = do
                             | x > 1 / bottomRowCols -> rescan
                             | x > 0 / bottomRowCols -> togglePower dev
                             | otherwise -> #lastError ?= OutOfRangeX x
-                    | otherwise -> #lastError ?= OutOfRangeY y
+                    | otherwise -> maybe (#lastError ?= OutOfRangeY y) setColour $ dims !? row
           where
-            rows = genericLength (filter cdSupported enumerate)
+            dims = filter cdSupported enumerate
+            rows = length dims
             setColour d = do
                 #dimension ?= d
                 setColourFromX dev' x

commit 255ae0b04d6270779a86ea8f8c91449b6bb2272d

Fix row number calculation
We were previously getting numbers too high for the special bottom row when said row is thicker than the colour dimension rows, i.e. when a device supports all dimensions.

This issue was probably introduced in e957c05.

src/Main.hs
@@ -420,9 +420,10 @@ update inc event = do
             -- TODO synchronise this with rendering
             let bottomRowCols = 3
                 bottomRowHeight = 1 / 4
-                row = rows - ceiling (fromIntegral rows * (y - bottomRowHeight) / (1 - bottomRowHeight))
-             in if row /= rows
-                    then maybe (#lastError ?= OutOfRangeY y) setColour $ dims !? row
+             in if y > bottomRowHeight
+                    then
+                        let row = rows - ceiling (fromIntegral rows * (y - bottomRowHeight) / (1 - bottomRowHeight))
+                         in maybe (#lastError ?= OutOfRangeY y) setColour $ dims !? row
                     else
                         if
                             | x > 3 / bottomRowCols -> #lastError ?= OutOfRangeX x